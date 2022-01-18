570fd6c8ebcb16533473c0aea6c4b78d0d470957
Nez Mustafa <nezmustafa123@gmail.com>
2022-01-18 14:58:41 +0000

    enable button if something in the input

--- end.js
+++ end.js
@@ -1,7 +1,10 @@
 const username = document.getElementById("username");
+const saveScoreBtn = document.getElementsByID("saveScoreBtn");
+
 username.addEventListener("keyup", () => {
   //keyup event listener
-  console.log(username.value);
+  //   console.log(username.value);
+  saveScoreBtn.disabled = !username.value; //disable button if nothing in the username input
 });
 
 saveHighScore = (e) => {
